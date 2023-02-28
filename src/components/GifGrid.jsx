ac8a17bde8c7808b5cb3dda5758107d006a801ca
Fernando <info_fotografia@icloud.com>
2023-02-28 16:24:42 +0100

    Despliegue en Netlify https://gif-expert-ferp7.netlify.app/

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,5 +1,3 @@
-import { useState, useEffect } from "react";
-// import { getGifs } from "../helpers/getGifs";
 import { GifItem } from "../components/GifItem";
 import { useFetchGifs } from "../hooks/useFetchGifs";
 
@@ -9,6 +7,7 @@ export const GifGrid = ({ category }) => {
   return (
     <>
       <h3>{category}</h3>
+      {isLoading && <h2>Cargando...</h2>}
       <div className="card-grid">
         {images.map((image) => (
           <GifItem key={image.id} {...image} />
